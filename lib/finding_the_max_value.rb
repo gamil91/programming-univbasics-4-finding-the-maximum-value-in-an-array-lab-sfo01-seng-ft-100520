df951d0e257bc5a3db718527f988255c7a6f0fe1
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-01 04:06:27 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -9,3 +9,13 @@ def find_max_value(array)
  end
  return max 
 end
+
+def find_max_value(array)
+ i = 0 
+ while i < array.length do
+  if array[i] == array.max 
+    return array[i]
+  end
+   i += 1 
+ end
+end
